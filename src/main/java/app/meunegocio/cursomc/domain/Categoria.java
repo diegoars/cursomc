8156661397921f9b4ae4f2884f0e386d0924adcb
diegoars <diegoalexandre.rs@gmail.com>
2018-07-16 22:46:57 -0300

    Add bd H2;criacao automatica de base de dados

--- src/main/java/app/meunegocio/cursomc/domain/Categoria.java
+++ src/main/java/app/meunegocio/cursomc/domain/Categoria.java
@@ -2,10 +2,18 @@ package app.meunegocio.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = -555212226439529618L;
-
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
